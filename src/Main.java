56332514810bfba6984220718b11cd3e9870541d
vladgoor <vladkruk2002@mail.ru>
2025-05-07 00:47:37 +0300

    add Task2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        int taskNumber = 2; // Номер задания
         int numOfArray = 10; // Кол-во элементов массива
         int[] arr = new int[numOfArray]; // Инициализация массива
         System.out.println("Массив " + numOfArray  + " элементов :" );
@@ -15,23 +16,66 @@ public class Main {
             System.out.print(arr[count] + " "); // Вывод всех эллементов массива
         }
         System.out.println(" ");
+        switch(taskNumber){ // Выбор задания
+
+
 
 //=============================Task 1=========================================
-        int maxNum;
-        int minNum;
+            case (1):
+                int maxNum;
+                int minNum;
 
-        if (numOfArray == 1) System.out.println("Массив из 1 элемента ==> max,min = " + arr[0]); // Проверка на массив из 1 элем.
-        maxNum = arr[0];
-        minNum = arr[0];
-        for(int count = 0; count < numOfArray; count++){
-            if (arr[count] < minNum){
-                minNum = arr[count];
-            }
-            if (arr[count] > maxNum) {
-                maxNum = arr[count];
-            }
+                if (numOfArray == 1)
+                    System.out.println("Массив из 1 элемента ==> max,min = " + arr[0]); // Проверка на массив из 1 элем.
+                maxNum = arr[0];
+                minNum = arr[0];
+                for (int count = 0; count < numOfArray; count++) {
+                    if (arr[count] < minNum) {
+                        minNum = arr[count];
+                    }
+                    if (arr[count] > maxNum) {
+                        maxNum = arr[count];
+                    }
+                }
+                System.out.println("min = " + minNum + " max = " + maxNum);
+            break;
+//=============================Task 2=========================================
+            case (2):
+                int evenNum = 0;
+                int notEvenNum = 0;
+
+                for (int count = 0; count < numOfArray; count++){ // Определяем размер массива для чет и не чет чисел
+                    if(arr[count] % 2 == 0) evenNum++; else notEvenNum++;
+                }
+                int[] arrEvenNum = new int[evenNum];
+                int indexArrEvenNum = 0;
+                int[] arrNotEvenNum = new int[notEvenNum];
+                int indexArrNotEvenNum = 0;
+
+                for (int count = 0; count < numOfArray; count++) {
+                    if (arr[count] % 2 == 0) {
+                        arrEvenNum[indexArrEvenNum] = arr[count];
+                        indexArrEvenNum++;
+                    }
+                }
+                for (int count = 0; count < numOfArray; count++) {
+                    if(arr[count] % 2 == 1  || arr[count] % 2 == -1) {
+                        arrNotEvenNum[indexArrNotEvenNum] = arr[count];
+                        indexArrNotEvenNum++;
+                    }
+                }
+
+                System.out.print("Четные числа :" );
+                for(int count = 0; count < indexArrEvenNum; count++){
+                    System.out.print(arrEvenNum[count] + " ");
+                }
+                System.out.println(" ");
+                System.out.print("Нечетные числа :" );
+                for(int count = 0; count < indexArrNotEvenNum; count++){
+                    System.out.print(arrNotEvenNum[count] + " ");
+                }
+            break;
         }
-        System.out.println("min = " + minNum + " max = " + maxNum);
     }
 
 }
