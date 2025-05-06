e4ad929f068d06e17c86e546d868b0d18bd91fe0
vladgoor <vladkruk2002@mail.ru>
2025-05-06 23:50:20 +0300

    finalTask1

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,18 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
-
         int numOfArray = 10; // Кол-во элементов массива
         int[] arr = new int[numOfArray]; // Инициализация массива
         System.out.println("Массив " + numOfArray  + " элементов :" );
+
 
         for(int count = 0; count < numOfArray; count++){ // Запись в каждый элемент массива рандомного числа
             Random rand = new Random();
             arr [count] = rand.nextInt(-100, 100) ; // Знач от -100 до 100
             System.out.print(arr[count] + " "); // Вывод всех эллементов массива
-
         }
         System.out.println(" ");
+
 //=============================Task 1=========================================
         int maxNum;
         int minNum;
@@ -33,7 +32,6 @@ public class Main {
             }
         }
         System.out.println("min = " + minNum + " max = " + maxNum);
-
     }
 
 }
